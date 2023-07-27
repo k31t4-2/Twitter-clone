49fb30cbafe26a977e99c5da1720b26760cf0e35
KEITA <ksekimotonz@gmail.com>
2023-07-27 21:11:21 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Sidebar from './Components/Sidebar/Sidebar'
+import Timeline from './Components/Timeline/Timeline'
+import Widget from './Components/Widget/Widget'
+
+function App() {
+
+  return (
+    <>
+      <Sidebar/>
+      <Timeline/>
+      <Widget/>
+
+    </>
+  )
+}
+
+export default App
